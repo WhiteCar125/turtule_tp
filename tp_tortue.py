214145a8e25f0be07ac0e9056a5c0a284842bc4c
Mael <lemenn-m08@saint-louis29.net>
2024-10-04 14:19:40 +0200

    Version 1

---
+++ tp_tortue.py
@@ -0,0 +1,24 @@
+import turtle
+t = turtle.Turtle()
+t.speed(0) # maximum speed
+def equilateral(longueur):
+    polygone(longueur,3)
+    
+def carre(longueur):
+    polygone(longueur,4)
+
+def polygone(longueur,nb_cote,ajout=0, deviation=0):
+    angle = (360/nb_cote) - deviation
+    for _ in range(nb_cote):
+        longueur += ajout
+        t.forward(longueur)
+        t.left(angle)
+        t.color("green", "red")
+
+def figure1():
+    polygone(0.5,360000,0.05,-15)
+
+carre
+
+        
+turtle.exitonclick()
